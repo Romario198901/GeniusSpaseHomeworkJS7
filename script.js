a8ca31db86ea9f92c7d3b2ec949183e5c4612647
Roman Hrydin <153761577+Romario198901@users.noreply.github.com>
2024-12-10 20:15:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Створіть функцію-конструктор Calculator, який створює об’єкти з трьома
+// методами:
+// read() запитує два значення за допомогою prompt і запам’ятовує їх у
+// властивостях об’єкта.
+// sum() повертає суму цих властивостей.
+// mul() повертає результат множення даних властивостей.
+
+
+// class Calculator {
+//     constructor(a,b){
+//         this.a = a;
+//        this.b = b;
+//     };
+//     read() {
+//         this.a = +prompt("Введіть перше число:", 0);
+//     this.b = +prompt("Введіть друге число:", 0);
+//     };
+//     sum() {
+//         return this.a + this.b;
+//     };
+//     mul() {
+//         return this.a * this.b;
+//     };
+// }
+
+// const calc = new Calculator;
+// calc.read();
+// alert(`Sum is  ${calc.sum()}`);
+// alert(`Mul is ${calc.mul()}`);
